4d1ad8bde1db08d9cd84a83e6345ddf6d49e3b19
2eonardo <leonardo.soldani3@edu.unifi.it>
2026-03-10 16:14:16 +0100

    Eliminazione file obsoleti

--- s_balneare/src/main/java/com/example/s_balneare/application/factory/UserFactory.java
+++
@@ -1,9 +0,0 @@
-package com.example.s_balneare.application.factory;
-
-import com.example.s_balneare.application.port.in.RegistrationRequest;
-import com.example.s_balneare.domain.user.User;
-
-//TODO: da riguardare correttezza e possibile eliminazione
-public abstract class UserFactory {
-    public abstract User createUser(RegistrationRequest request);
-}
