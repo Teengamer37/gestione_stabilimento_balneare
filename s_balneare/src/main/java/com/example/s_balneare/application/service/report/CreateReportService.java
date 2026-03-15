226d495d36872beeb5f27729a5441796931a7d11
2eonardo <leonardo.soldani3@edu.unifi.it>
2026-03-15 22:05:06 +0100

    Correzione UC-09
    TODO: Progetto SWE
    ******** DA FARE ********
    - (UC-01) spostare ManageUserService.logIn() su una nuova classe AuthenticationService; implementare prima BanRepository (guarda TODO in ManageUserService.java:29)
    - (UC-05, UC-06) verificare se Customer/Owner non siano bannati dalla piattaforma
    - (UC-07) aggiungere controllo se utente bannato dalla spiaggia alla creazione della Review
    - Verificare se il codice rispecchia tutti gli Use Cases
    - Aggiungere TODOs
    - Risolvere i nuovi TODOs
    - AGGIORNARE CLASSI DOMAIN IN UML
    - Pulizia codice:
    	Classe ban, può essere un record
    - Aggiungere metodo unico per esecuzione di una query per le classi jdbc

--- s_balneare/src/main/java/com/example/s_balneare/application/service/report/CreateReportService.java
+++ s_balneare/src/main/java/com/example/s_balneare/application/service/report/CreateReportService.java
@@ -2,6 +2,7 @@ package com.example.s_balneare.application.service.report;
 
 import com.example.s_balneare.application.port.in.report.CreateReportCommand;
 import com.example.s_balneare.application.port.in.report.CreateReportUseCase;
+import com.example.s_balneare.application.port.out.BanRepository;
 import com.example.s_balneare.application.port.out.ReportRepository;
 import com.example.s_balneare.application.port.out.TransactionManager;
 import com.example.s_balneare.application.port.out.beach.BeachRepository;
@@ -13,6 +14,7 @@ import com.example.s_balneare.domain.booking.BookingStatus;
 import com.example.s_balneare.domain.moderation.Report;
 import com.example.s_balneare.domain.moderation.ReportStatus;
 import com.example.s_balneare.domain.moderation.ReportTargetType;
+import com.example.s_balneare.domain.user.Customer;
 import com.example.s_balneare.domain.user.Role;
 import com.example.s_balneare.domain.user.User;
 
@@ -26,13 +28,15 @@ public class CreateReportService<T extends User> implements CreateReportUseCase 
     private final UserRepository<T> userRepository;
     private final BookingRepository bookingRepository;
     private final BeachRepository beachRepository;
+    private final BanRepository banRepository;
     private final TransactionManager transactionManager;
 
-    public CreateReportService(ReportRepository reportRepository, UserRepository<T> userRepository, BookingRepository bookingRepository, BeachRepository beachRepository, TransactionManager transactionManager) {
+    public CreateReportService(ReportRepository reportRepository, UserRepository<T> userRepository, BookingRepository bookingRepository, BeachRepository beachRepository, BanRepository banRepository, TransactionManager transactionManager) {
         this.reportRepository = reportRepository;
         this.userRepository = userRepository;
         this.bookingRepository = bookingRepository;
         this.beachRepository = beachRepository;
+        this.banRepository = banRepository;
         this.transactionManager = transactionManager;
     }
 
@@ -69,35 +73,43 @@ public class CreateReportService<T extends User> implements CreateReportUseCase 
             if(reporterUser.getRole() == Role.ADMIN) {
                 throw new IllegalStateException("ERROR: Invalid type user");
             }
-
-            //4: Recupero dati spiaggia del booking
+            //Controllo l'utente non sia bannato dall'app, identico che controllare se l'utente è attivo
+            //Uso ban per controllare così il controllo viene effettutato sia se è un customer che un owner
+            if (banRepository.isBannedFromApp(command.reporterId(), context)) {
+                throw new IllegalStateException("ERROR: The user has been banned");
+            }
+            //4: Recupero dati spiaggia del booking e controllo
             Beach beach = beachRepository.findById(booking.getBeachId(), context)
                     .orElseThrow(() -> new IllegalArgumentException("ERROR: Beach does not exist"));
+            //Controllo spiaggia non sia chiusa, se è disattivata perchè in manutenzione posso lasciare una recensione
+            if (beach.isClosed()) {
+                throw new IllegalStateException("ERROR: Beach is not active or closed");
+            }
 
             //5: logica di business e sicurezza
             ReportTargetType reportedType;
             Integer reportedId;
 
-            if (reporterUser.getRole() == Role.CUSTOMER ){
+            if (reporterUser instanceof Customer customer){
                 //Customer segnala la spiaggia
+                //Controllo segnali spiagge inerenti alla prenotazione
                 if(!Objects.equals(command.reporterId(), booking.getCustomerId())) {
                     throw new IllegalStateException("ERROR: You are not the owner of this reservation");
                 }
-                if (!beach.isActive()) {
-                    throw new IllegalStateException("ERROR: Beach is not active");
+                //Controllo che il customer che fa il report non sia bannato dalla spiaggia
+                if (banRepository.isBannedFromBeach(command.reporterId(), beach.getId(), context)) {
+                    throw new IllegalStateException("ERROR: Your are banned from this beach");
                 }
                 reportedType = ReportTargetType.BEACH;
                 reportedId = beach.getOwnerId();
             } else{ //Owner segnala il customer
                 if (!Objects.equals(command.reporterId(), beach.getOwnerId())){
-                    throw new IllegalStateException("ERROR: You can only report customers of your own beach");
+                    throw new IllegalStateException("ERROR: You can only report bookings of your own beach");
                 }
                 reportedType = ReportTargetType.USER;
                 reportedId = booking.getCustomerId();
             }
-            //TODO: Passo 3 controllo che il customer non sia bannato ne dalla spiaggia e ne dall'app
-            // da fare dopo aver implementato ban e l'attributo closed
-
+            
             Report newReport = new Report(
                     0,
                     command.reporterId(),
